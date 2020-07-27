5f6f4b9489f9e27e03ffb2754298c30d3d04c363
FragAverage <54283228+FragAverage@users.noreply.github.com>
2020-07-27 03:58:06 +0100

    Update funcs.lua

--- NotyFive/client/funcs.lua
+++ NotyFive/client/funcs.lua
@@ -1,100 +1,10 @@
---[[
-Complete List of Options:
-        type
-        layout
-        theme
-        text
-        timeout
-        progressBar
-        closeWith
-        animation = {
-            open
-            close
-        }
-        sounds = {
-            volume
-            conditions
-            sources
-        }
-        docTitle = {
-            conditions
-        }
-        modal
-        id
-        force
-        queue
-        killer
-        container
-        buttons
+--[[ 
+    
+https://ned.im/noty/#/ 
 
-More details below or visit the creators website http://ned.im/noty/options.html
 
-Layouts:
-    top
-    topLeft
-    topCenter
-    topRight
-    center
-    centerLeft
-    centerRight
-    bottom
-    bottomLeft
-    bottomCenter
-    bottomRight
 
-Types:
-    alert
-    success
-    error
-    warning
-    info
-
-Themes: -- You can create more themes inside html/themes.css, use the gta theme as a template.
-    gta
-    mint
-    relax
-    metroui
-
-Animations:
-    open:
-        noty_effects_open
-        gta_effects_open
-        gta_effects_open_left
-        gta_effects_fade_in
-    close:
-        noty_effects_close
-        gta_effects_close
-        gta_effects_close_left
-        gta_effects_fade_out
-
-closeWith: -- array, You will probably never use this.
-    click
-    button
-
-sounds:
-    volume: 0.0 - 1.0
-    conditions: -- array
-        docVisible
-        docHidden
-    sources: -- array of sound files
-
-modal:
-    true
-    false
-
-force:
-    true
-    false
-
-queue: -- default is global, you can make it what ever you want though.
-    global
-
-killer: -- will close all visible notifications and show only this one
-    true
-    false
-
-visit the creators website https://ned.im/noty/#/options for more information
---]]
+]]
 
 function SetQueueMax(queue, max)
     local tmp = {
